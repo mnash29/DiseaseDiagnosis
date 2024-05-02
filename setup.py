ea0ff0bc821142ac67371ede0954400db900048a
mnash29 <mtntrades10@gmail.com>
2024-05-02 13:03:08 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A computer vision model to detect the presence of malaria in image data',
+    author='Matthew Nash',
+    license='MIT',
+)
